5362639ce0d6a764b6dbab673c46d01cb7da3bf1
albornet <alban.bornet@gmail.com>
2023-06-28 09:11:06 +0200

    added sbatch scripts

--- run_gnn.sh
+++ run_gnn.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-#SBATCH --job-name=run_controls
+#SBATCH --job-name=run_gnn
 #SBATCH --partition=private-teodoro-gpu
-#SBATCH --gpus=1
+#SBATCH --gpus=8
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=12
 #SBATCH --mem=32gb
 #SBATCH --time=0-24:00:00
-#SBATCH --output=logs/run_controls_%j.txt
-#SBATCH --error=logs/run_controls_%j.err
+#SBATCH --output=logs/run_gnn_%j.txt
+#SBATCH --error=logs/run_gnn_%j.err
 
 REGISTRY=/home/users/b/borneta/sif
 SIF=dl.sif
 IMAGE=${REGISTRY}/${SIF}
-SCRIPT=run_controls.py
+SCRIPT=run_gnn.py
 
 srun apptainer run --nv ${IMAGE} python ${SCRIPT}
